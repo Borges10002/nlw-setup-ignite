4d8b31119d0974cc46c263cdcbdaab7e02da699c
unknown <borges10002@gmail.com>
2023-01-22 11:39:25 -0400

    finalizado mobile aula 3

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,18 +1,19 @@
-import React from "react";
 import { View, Text, ScrollView } from "react-native";
 
 import { generateRangeDatesFromYearStart } from "../utils/generate-range-between-dates";
 
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { useNavigation } from "@react-navigation/native";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
-
 const datesFromYearStart = generateRangeDatesFromYearStart();
-const minimumSummaryDatesSizes = 18 * 7;
-const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
+const minimunSummaryDatesSizes = 18 * 5;
+const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className='flex-1 bg-background px-8 pt-16'>
       <Header />
@@ -32,7 +33,10 @@ export function Home() {
         contentContainerStyle={{ paddingBottom: 100 }}>
         <View className='flex-row flex-wrap'>
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate("habit", { date: date.toISOString() })}
+            />
           ))}
 
           {amountOfDaysToFill > 0 &&
@@ -40,6 +44,7 @@ export function Home() {
               length: amountOfDaysToFill,
             }).map((_, index) => (
               <View
+                key={index}
                 className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40'
                 style={{ width: DAY_SIZE, height: DAY_SIZE }}
               />
