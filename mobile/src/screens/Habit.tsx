a86c9d15ee9a66583b9d6bb5e5c5c4e1c37c3894
Borges - 746 <borges@ciclocairu.com.br>
2023-01-23 21:51:17 -0400

    aula5 mobile

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,16 +1,21 @@
-import { ScrollView, View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, View, Text, Alert } from "react-native";
 import dayjs from "dayjs";
 
 import { useRoute } from "@react-navigation/native";
 import { BackButton } from "../components/BackButton";
 import { ProgressBar } from "../components/ProgressBar";
 import { Checkbox } from "../components/Checkbox";
+import { Loading } from "../components/Loading";
+import { api } from "../lib/axios";
 
 interface Params {
   date: string;
 }
 
 export function Habit() {
+  const [loading, setLoading] = useState(true);
+
   const route = useRoute();
   const { date } = route.params as Params;
 
@@ -18,25 +23,50 @@ export function Habit() {
   const dayOfWeeek = parsedDate.format("dddd");
   const dayAndMonth = parsedDate.format("DD/MM");
 
+  async function fetchHabits() {
+    try {
+      setLoading(true);
+
+      const response = await api.get("/day", { params: { date } });
+    } catch (error) {
+      console.log(error);
+      Alert.alert(
+        "Ops",
+        "Não foi possivel carregar as informações dos hábitos"
+      );
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchHabits();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
-    <View className='flex-1 bg-background px-8 pt-16'>
+    <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 100 }}>
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
         <BackButton />
-        <Text className='mt-6 text-zinc-400 font-semibold text-base lowercase'>
+        <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
           {dayOfWeeek}
         </Text>
 
-        <Text className='text-white font-extrabold text-3xl'>
+        <Text className="text-white font-extrabold text-3xl">
           {dayAndMonth}
         </Text>
 
         <ProgressBar progress={45} />
 
-        <View className='mt-6'>
-          <Checkbox title='Beber agua todos os dias' checked={false} />
-          <Checkbox title='testeeeeeeee' checked={false} />
+        <View className="mt-6">
+          <Checkbox title="Beber agua todos os dias" checked={false} />
+          <Checkbox title="testeeeeeeee" checked={false} />
         </View>
       </ScrollView>
     </View>
--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -42,7 +42,7 @@ export function New() {
   async function hangleCreateNewHabit() {
     try {
       if (!title.trim() || weekDays.length === 0) {
-        Alert.alert(
+        return Alert.alert(
           "Novo Hábito ",
           "Informe o nome do hábito e escolha a periodicidade"
         );
@@ -61,30 +61,31 @@ export function New() {
   }
 
   return (
-    <View className='flex-1 bg-background px-8 pt-16'>
+    <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView
         showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 100 }}>
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
         <BackButton />
 
-        <Text className='mt-6 text-white font-extrabold text-3xl'>
+        <Text className="mt-6 text-white font-extrabold text-3xl">
           Criar hábito
         </Text>
 
-        <Text className='mt-6 text-white font-semibold text-base'>
+        <Text className="mt-6 text-white font-semibold text-base">
           Qual seu comprometimento?
         </Text>
 
         <TextInput
-          className='h-12 pl-4 rounded-lg mt-3
-           bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600'
-          placeholder='Execícios, domir bem, etc...'
+          className="h-12 pl-4 rounded-lg mt-3
+           bg-zinc-900 text-white border-2 border-zinc-800 focus:border-green-600"
+          placeholder="Execícios, domir bem, etc..."
           placeholderTextColor={colors.zinc[400]}
           onChangeText={setTitle}
           value={title}
         />
 
-        <Text className='font-semibold mt-4 mb-3 text-white text-base'>
+        <Text className="font-semibold mt-4 mb-3 text-white text-base">
           Qual a recorrência?
         </Text>
 
@@ -98,11 +99,12 @@ export function New() {
         ))}
 
         <TouchableOpacity
-          className='w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6'
+          className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
           activeOpacity={0.7}
-          onPress={hangleCreateNewHabit}>
-          <Feather name='check' size={20} color={colors.white} />
-          <Text className='font-semibold text-base text-white ml-2'>
+          onPress={hangleCreateNewHabit}
+        >
+          <Feather name="check" size={20} color={colors.white} />
+          <Text className="font-semibold text-base text-white ml-2">
             Confirmar
           </Text>
         </TouchableOpacity>
